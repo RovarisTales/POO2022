60e4c2a87a78ff057159aa1d24a8d37ab0747941
Tiago Rodrigues <tiagopassosrodrigues@hotmail.com>
2022-04-11 14:46:57 +0100

    Comercializador

---
+++ src/ComercializadorEnergia.java
@@ -0,0 +1,9 @@
+public class ComercializadorEnergia {
+    private String nome;
+    private double custoDiarioEner;
+    private static double IMPOSTO = 2.0;
+
+    public double preçoDiaDisp(double consumoDisp){
+        return 0;
+    }
+}
